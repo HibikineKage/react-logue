75c80fc70a96d0f13df8a982d2f503c7692223af
Hibikine Kage <goodsmile314159@gmail.com>
2019-06-12 18:05:34 +0900

    add jesct config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**'
+  ],
+  testEnvironment: 'node'
 };
